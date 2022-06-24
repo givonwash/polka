da869eedbec7146126112f51499ee7006304e6f1
givonwash <givonwash@gmail.com>
2022-06-23 23:23:28 -0700

    fix(givon, env): hide wayland-specific env vars behind `lib.mkIf` call

--- users/givon/wayland/default.nix
+++ users/givon/wayland/default.nix
@@ -1,11 +1,14 @@
-{ me, utils, ... }: { pkgs, ... }:
+{ me, utils, ... }: { config, lib, ... }:
 
+let
+  cfg = config._.${me};
+in
 {
   imports = [
     (import ./sway { inherit me utils; })
   ];
 
-  home-manager.users.${me} = { config, pkgs, ... }: {
+  home-manager.users.${me} = lib.mkIf cfg.sway.enable {
     home.sessionVariables.MOZ_ENABLE_WAYLAND = 1;
     home.sessionVariables.MOZ_DBUS_REMOTE = 1;
   };
