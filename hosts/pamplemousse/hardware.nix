bab0ff0250c8b1e3e4b77bfcbe6693875a831235
givonwash <givonwash@gmail.com>
2022-08-31 14:10:27 -0700

    chore(pamplemousse): reformat network interfaces configuration

--- hosts/pamplemousse/hardware.nix
+++ hosts/pamplemousse/hardware.nix
@@ -14,9 +14,7 @@
   networking = {
     networkmanager.enable = true;
     useDHCP = false;
-    interfaces = {
-      wlp0s20f3.useDHCP = true;
-    };
+    interfaces.wlp0s20f3.useDHCP = true;
   };
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
