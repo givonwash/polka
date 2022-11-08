dcaa960e14bc88d988dfceb4aaebecdcaa118661
givonwash <givonwash@gmail.com>
2022-11-07 20:59:39 -0800

    feat(givon, nvim): add keymaps for obsidian-nvim & specify notes_subdir

--- users/givon/nvim/lua/config/plugins/obsidian-nvim.lua
+++ users/givon/nvim/lua/config/plugins/obsidian-nvim.lua
@@ -3,6 +3,7 @@ return {
     ---@return nil
     setup = function(opts)
         local obsidian = require 'obsidian'
+        local keymaps = require 'core.keymaps'
 
         opts.vault = vim.fs.normalize(opts.vault)
 
@@ -20,6 +21,14 @@ return {
                     return time .. 'untitled'
                 end
             end,
+            notes_subdir = 'canvas',
+        }
+
+        keymaps.define {
+            n = {
+                ['<leader>fn'] = '<cmd>ObsidianSearch<cr>',
+                ['<leader>nn'] = { cmd = ':ObsidianNew<space>', opts = { silent = false } },
+            },
         }
 
         require('core.autocmds').define_group('ObsidianVaultSetup', {
@@ -31,7 +40,7 @@ return {
                         local file = args.file
 
                         if file:find('^' .. opts.vault) ~= nil then
-                            require('core.keymaps').define {
+                            keymaps.define {
                                 n = {
                                     ['gf'] = {
                                         cmd = function()
@@ -42,6 +51,17 @@ return {
                                             end
                                         end,
                                         opts = { buffer = true, expr = true, silent = false },
+                                    },
+                                    ['<leader>nb'] = '<cmd>ObsidianBacklinks<cr>',
+                                },
+                                v = {
+                                    ['<leader>nl'] = {
+                                        cmd = ':ObsidianLink<space>',
+                                        opts = { silent = false },
+                                    },
+                                    ['<leader>nn'] = {
+                                        cmd = ':ObsidianLinkNew<space>',
+                                        opts = { silent = false },
                                     },
                                 },
                             }
