83514c09cc96da64835265fe883685ad31e11807
givonwash <givonwash@gmail.com>
2022-12-20 11:00:27 -0800

    feat(givon, sway, mako): resize notifs, add dnd, & auto-dismiss spotify

--- users/givon/wayland/sway/mako.nix
+++ users/givon/wayland/sway/mako.nix
@@ -14,17 +14,25 @@ in
       backgroundColor = elemAt theme.colors.blacks 0;
       borderColor = theme.colors.peach;
       extraConfig = ''
+        [app-name=Spotify]
+        default-timeout=3000
+
         [urgency=high]
         border-color=${theme.colors.red}
 
         [grouped=true]
         format=<span color='${theme.colors.rosewater}' weight='bold'>(%g) %a: %s</span>\n%b
+
+        [mode=do-not-disturb]
+        invisible=1
       '';
       font = "${theme.fonts.sans-serif.name} 12";
       format = "<span color='${theme.colors.rosewater}' weight='bold'>%a: %s</span>\\n%b";
+      height = 150;
       layer = "overlay";
       progressColor = "over ${theme.colors.green}";
       textColor = "${theme.colors.white}";
+      width = 400;
     };
 
     wayland.windowManager.sway.config.keybindings =
@@ -32,8 +40,10 @@ in
         inherit (cfg.sway.keys) modifier;
       in
       {
-        "${modifier}+d" = "exec ${pkgs.mako}/bin/makoctl dismiss -n";
+        "${modifier}+d" = "exec ${pkgs.mako}/bin/makoctl dismiss";
         "${modifier}+shift+d" = "exec ${pkgs.mako}/bin/makoctl dismiss --all";
+        "${modifier}+z" = "exec ${pkgs.mako}/bin/makoctl mode -a do-not-disturb";
+        "${modifier}+shift+z" = "exec ${pkgs.mako}/bin/makoctl mode -r do-not-disturb";
       };
   };
 }
