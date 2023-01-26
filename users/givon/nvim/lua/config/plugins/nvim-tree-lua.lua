dfc766803192156352e3ddc67eadcbf79a9ea0af
givonwash <givonwash@gmail.com>
2023-01-26 11:53:52 -0800

    feat(givon, nvim): make nvim-tree float when toggled

--- users/givon/nvim/lua/config/plugins/nvim-tree-lua.lua
+++ users/givon/nvim/lua/config/plugins/nvim-tree-lua.lua
@@ -2,11 +2,39 @@ return {
     ---@return nil
     setup = function()
         require('nvim-tree').setup {
+            renderer = {
+                group_empty = true,
+            },
             respect_buf_cwd = true,
             update_cwd = true,
             update_focused_file = {
                 enable = true,
                 update_cwd = true,
+            },
+            view = {
+                float = {
+                    enable = true,
+                    open_win_config = function()
+                        local cmdheight = vim.opt.cmdheight:get()
+                        local screen_width = vim.opt.columns:get()
+                        local screen_height = vim.opt.lines:get()
+                        local effective_screen_height = screen_height - cmdheight
+
+                        local window_width = math.floor(screen_width * 0.8)
+                        local window_height = math.floor(effective_screen_height * 0.8)
+                        local column = (screen_width - window_width) / 2
+                        local row = ((screen_height - window_height) / 2) - cmdheight
+
+                        return {
+                            border = 'rounded',
+                            relative = 'editor',
+                            col = column,
+                            row = row,
+                            height = window_height,
+                            width = window_width,
+                        }
+                    end,
+                },
             },
         }
         require('core.keymaps').define {
