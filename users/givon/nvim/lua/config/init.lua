9c9d43e9c0247ed09577cc812d3a23d3eacb56da
givonwash <givonwash@gmail.com>
2023-05-25 18:40:19 -0700

    feat(givon, nvim): set `sw` to `2` when `ft=terraform`

--- users/givon/nvim/lua/config/init.lua
+++ users/givon/nvim/lua/config/init.lua
@@ -24,6 +24,15 @@ return {
                 },
             },
             {
+                event = 'FileType',
+                opts = {
+                    callback = function()
+                        vim.opt_local.shiftwidth = 2
+                    end,
+                    pattern = 'terraform',
+                },
+            },
+            {
                 event = 'BufWritePre',
                 opts = {
                     command = 'mark ` | %s:\\v\\s+$::ge | normal! ``',
