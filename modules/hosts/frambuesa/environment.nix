61a8ef7b8979d85d96bb20e885bdbf043594e97b
Givon Washington <givonwash@gmail.com>
2024-05-15 13:47:30 -0700

    chore(frambuesa): remove default `git` and `vim` packages

--- modules/hosts/frambuesa/environment.nix
+++ modules/hosts/frambuesa/environment.nix
@@ -1,12 +1,8 @@
-{ pkgs, ... }:
-
 {
   documentation.man = {
     generateCaches = true;
     man-db.enable = true;
   };
-
-  environment.systemPackages = with pkgs; [ git vim ];
 
   nix.gc = {
     automatic = true;
