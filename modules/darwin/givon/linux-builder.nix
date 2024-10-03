eb994f34ce27b3e379869b48880d8a7b47f3c03b
givonwash <givonwash@gmail.com>
2024-10-03 08:45:45 -0700

    chore(darwin, givon): make usage of `linux-builder` optional

--- modules/darwin/givon/linux-builder.nix
+++ modules/darwin/givon/linux-builder.nix
@@ -1,5 +1,9 @@
-{
-  nix = {
+{ config ,lib, ... }:
+let
+  cfg = config._.givon.linux-builder;
+in {
+  options._.givon.linux-builder.enable = lib.mkEnableOption "linux-builder";
+  config.nix = lib.mkIf cfg.enable {
     linux-builder = {
       enable = true;
       maxJobs = 6;
